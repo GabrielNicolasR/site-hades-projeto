22789bcffb1220bed574413e2b19df9b0924a880
Gabriel Nicolas <gabrielrochasa@gmail.com>
2024-04-01 12:16:07 -0300

    Enviado arquivos do projeto do site Hades

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const selecionarPlataforma = document.querySelector('.btn-plataforma');
+
+const elementoPlataformas = document.querySelector('.btn-plataforma .plataformas');
+
+selecionarPlataforma.addEventListener("click", () => {
+   elementoPlataformas.classList.toggle("ativo") 
+})
+
+
+
+
+
+
